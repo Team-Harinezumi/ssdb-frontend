cffa60ce75d0c7959c6d90cfcede0f1ee7f3af40
okb-okb <50895997+okb-okb@users.noreply.github.com>
2021-03-15 01:28:14 +0900

    最初のコミット by okb-okb (#3)

---
+++ pages/sample_okb.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from "next";
+import Link from "next/link";
+
+const SamplePage: NextPage = () => (
+  <>
+    <h1>Sample Page!!!!!!</h1>
+    <div>サンプルページ by okb-okb</div>
+    <Link href="/">Top</Link>
+  </>
+);
+
+export default SamplePage;
