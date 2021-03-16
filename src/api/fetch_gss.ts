a0627b31f2ff80da0f21e53ec572a484f64870ee
mitsueda <mitsueda.itsuki.bc@gmail.com>
2021-03-17 02:58:12 +0900

    GSSのデータを取ってくる部分

--- src/api/fetch_gss.ts
+++ src/api/fetch_gss.ts
@@ -1,2 +1,16 @@
-// WIP
-export {}
+import axios from 'axios'
+import { isGssUrl, createGssCsvUrl } from '../lib/gss_url'
+
+// csv形式でGSSのデータを取ってくる
+export const fetchGss = async (url: string) => {
+  try {
+    if (isGssUrl(url)) {
+      const adjustedUrl = createGssCsvUrl(url)
+      const res = await axios.get(adjustedUrl)
+      return res.data
+    }
+    return ''
+  } catch(e) {
+    return ''
+  }
+}
