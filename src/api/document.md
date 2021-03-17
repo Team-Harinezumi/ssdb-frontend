26c402049cf8f049fcfdbc0bfa5f1d6011c5a2c2
mitsueda-itsuki <63278411+mitsueda-itsuki@users.noreply.github.com>
2021-03-17 23:04:41 +0900

    スプレッドシートからデータを取ってくる & データ整形 (#6)
    * api/fetch_gss.ts を作成
    
    * axios を追加
    
    * gssのURLの形式をdoc.mdにまとめた
    
    * rename
    
    * GSSのデータを取ってくる部分
    
    * csvを配列形式に変換

---
+++ src/api/document.md
@@ -0,0 +1,46 @@
+# URLの形式一覧
+
+※ `ID`, `ID2` はファイル毎のユニークな値
+※ `ID` と `ID2` は同じファイルでも違う値です！
+
+## 編集リンク
+
+### 1枚目のシート
+
+`https://docs.google.com/spreadsheets/d/ID/edit#gid=0`
+
+### 2枚目以降のシート
+
+`https://docs.google.com/spreadsheets/d/ID/edit#gid=xxx`
+
+`xxx` の部分は勝手にできる
+
+### 共有リンク
+
+`https://docs.google.com/spreadsheets/d/ID/edit?usp=sharing`
+
+## ウェブに公開済
+
+## ドキュメント全体 / ウェブページ
+
+`https://docs.google.com/spreadsheets/d/e/ID2/pubhtml`
+
+## ドキュメント全体 / csv
+
+`https://docs.google.com/spreadsheets/d/e/ID2/pub?output=csv`
+
+## シート1 / ウェブページ
+
+`https://docs.google.com/spreadsheets/d/e/ID2/pubhtml?gid=0&single=true`
+
+## シート1 / csv
+
+`https://docs.google.com/spreadsheets/d/e/ID2/pub?gid=0&single=true&output=csv`
+
+## シートN / ウェブページ (N >= 2)
+
+`https://docs.google.com/spreadsheets/d/e/ID2/pubhtml?gid=2003195246&single=true`
+
+## シートN / csv (N >= 2)
+
+`https://docs.google.com/spreadsheets/d/e/ID2/pub?gid=xxx&single=true&output=csv`
---
+++ src/lib/gss_url.ts
@@ -0,0 +1,40 @@
+const GSS_ORIGIN = 'https://docs.google.com'
+const SPREADSHEETS = 'spreadsheets'
+
+export const isGssUrl = (url: string): boolean => {
+  try {
+    const urlObj = new URL(url)
+  
+    // オリジンの確認
+    if (urlObj.origin !== GSS_ORIGIN) {
+      return false
+    }
+  
+    // pathnameの先頭がspreadsheetsか確認
+    if (urlObj.pathname.slice(1, 13) !== SPREADSHEETS) {
+      return false
+    }
+  
+    return true
+  } catch(e) {
+    return false
+  }
+}
+
+export const createGssCsvUrl = (url: string): string => {
+  try {
+    const urlObj = new URL(url)
+  
+    // 公開URLでない場合は '' を返す
+    if (urlObj.pathname.slice(13, 18) !== '/d/e/') {
+      return ''
+    }
+  
+    // TODO: ドキュメント全体のURLが渡された場合は、何枚目のシートを使うか入力させるモーダル等が必要。一旦1枚目にしてます。
+    const uniqueId = (urlObj.pathname.split('/'))[4]
+    const gid = urlObj.searchParams.get('gid') || '0'
+    return `https://docs.google.com/spreadsheets/d/e/${uniqueId}/pub?gid=${gid}&single=true&output=csv`
+  } catch(e) {
+    return ''
+  }
+}
