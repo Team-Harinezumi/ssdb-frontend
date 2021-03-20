cdfddd4108e7f1572ba443e7a173f6410f50389b
okb-okb <50895997+okb-okb@users.noreply.github.com>
2021-03-20 19:10:53 +0900

    データをCSVに変換する関数、ヘッダ行指定に対応 (#14)
    * ヘッダの開始行を変更可能にする
    
    * ヘッダの開始行を変更可能にする際のバグ修正

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -8,12 +8,26 @@ const SearchForm: NextPage = () => {
   const [inputUrl, setInputUrl] = useState(
     "https://docs.google.com/spreadsheets/d/e/2PACX-1vSDSvWQNtJMW5IUsLF6FP12PNt8nSqaqw554UiNnUEYAZlWSp7PU509-M2IJ96D72gpCJznDvyied57/pubhtml"
   );
+  // ヘッダ行のインデックス(ユーザ入力)
+  const [inputIndex, setInputIndex] = useState(1);
+  // ヘッダ行のインデックス(次のコンポーネントに渡す)
+  // inputIndexと分けないとデータの表示中にボタンを触った時想定外の挙動
+  const [headerIndex, setHeaderIndex] = useState(1);
   // SSの整形前のデータ
   const [sheet, setSheet] = useState<string[][]>([]);
 
   // ユーザの入力を取得
   const handleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
     setInputUrl(event.target.value as string);
+  };
+  // インデックスを1増やす
+  const handleIncrement = () => {
+    setInputIndex((inputIndex) => inputIndex + 1);
+  };
+  // インデックスを1減らす
+  const handleDecrement = () => {
+    if (inputIndex === 1) return;
+    setInputIndex((inputIndex) => inputIndex - 1);
   };
 
   // 検索ボタンを押すと入力したURLからSSが呼ばれ、シートを取得(未整形)
@@ -23,6 +37,7 @@ const SearchForm: NextPage = () => {
       setSheet(fetchedGss as string[][]);
     };
     gss();
+    setHeaderIndex(inputIndex);
   };
 
   return (
@@ -34,7 +49,11 @@ const SearchForm: NextPage = () => {
         onChange={handleChange}
       />
       <button onClick={handleSearch}>検索</button>
-      <FilteringBox rawData={sheet} />
+      ヘッダの開始行
+      <button onClick={handleDecrement}>-</button>
+      {inputIndex}
+      <button onClick={handleIncrement}>+</button>
+      <FilteringBox rawData={sheet} headerIndex={headerIndex} />
     </>
   );
 };
