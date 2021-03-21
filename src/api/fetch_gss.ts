cbc5cc51d51416329696bb18318d6465a1a3e442
mitsueda-itsuki <63278411+mitsueda-itsuki@users.noreply.github.com>
2021-03-21 18:40:12 +0900

    任意シートについて、シート毎の名前 + gid を取れるように あとタイトル (#16)
    * 任意シートについて、シート毎の名前 + gid を取れるように あとタイトル
    
    * 一旦anyで逃げる
    
    * クローンの仕方を変更

--- src/api/fetch_gss.ts
+++ src/api/fetch_gss.ts
@@ -1,12 +1,12 @@
 import axios from 'axios'
 import csvtojson from 'csvtojson'
-import { isGssUrl, createGssCsvUrl } from '../lib/gss_url'
+import { isGssUrl, createGssCsvUrl, createGssPubHtmlUrl } from '../lib/gss_url'
 
 // GSSのデータを取ってくる
-export const fetchGss = async (url: string) => {
+export const fetchGss = async (url: string, gidParam?: string) => {
   try {
     if (isGssUrl(url)) {
-      const adjustedUrl = createGssCsvUrl(url)
+      const adjustedUrl = createGssCsvUrl(url, gidParam)
       const res = await axios.get(adjustedUrl)
       return csvtojson({
         noheader: true,
@@ -17,5 +17,31 @@ export const fetchGss = async (url: string) => {
     return ''
   } catch(e) {
     return ''
+  }
+}
+
+// シートの情報を取ってくる
+export const fetchSheetsInfo = async (url: string) => {
+  try {
+    if (isGssUrl(url)) {
+      const adjustedUrl = createGssPubHtmlUrl(url)
+      const res = await axios.get(adjustedUrl)
+      const domParser = new DOMParser()
+      const dom = domParser.parseFromString(res.data, "text/html")
+      const sheetMenu = Array.from(dom.getElementById('sheet-menu')?.getElementsByTagName('li') || [])
+      const sheetInfo = sheetMenu.map(node => (
+        {
+          gid: node.id.split('-')[2],
+          sheetName: node.innerText
+        }
+      ))
+      return {
+        title: dom.title.slice(0,-14),
+        data: sheetInfo
+      }
+    }
+
+  } catch(e) {
+    return []
   }
 }
