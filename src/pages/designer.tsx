e8a822c83dcc4c38d29e2c4012d777ccba6ffbee
okb-okb <kosukeokabe1998@gmail.com>
2021-03-23 17:00:10 +0900

    魔法のスプレッド専用シートを追加、react-boot-strapを導入

---
+++ src/pages/designer.tsx
@@ -0,0 +1,8 @@
+import { NextPage } from "next";
+import MagicSpreadSheet from "@/components/MagicSpreadSheet";
+
+const Designer: NextPage = () => {
+  return <MagicSpreadSheet type={"designer"} />;
+};
+
+export default Designer;
